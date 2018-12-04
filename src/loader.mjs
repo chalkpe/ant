30fe12997dfabfa1120e9353da3ae91832c1a196
Chalk <chalkpe@gmail.com>
2018-12-04 16:00:14 +0900

    small refactor

--- src/loader.mjs
+++ src/loader.mjs
@@ -3,7 +3,6 @@ import { join } from 'path'
 import { homedir } from 'os'
 import go from './go'
 
-const read = fs.promises.readFile
 const valid = s => s => s.name && s.path && s.rule
 
 export default async function load (dir = homedir()) {
@@ -19,7 +18,7 @@ export default async function load (dir = homedir()) {
     }
   }
 
-  const [str, err] = await go(read(rc, 'utf8'))
+  const [str, err] = await go(fs.promises.readFile(rc, 'utf8'))
   if (err || !str) return def
 
   const [obj, errr] = await go(_ => JSON.parse(str))
@@ -29,5 +28,5 @@ export default async function load (dir = homedir()) {
   if (!target || !valid(target)) return def
   if (!Array.isArray(sources) || !sources.every(valid)) return def
 
-  return {sources, target}
+  return { sources, target }
 }
