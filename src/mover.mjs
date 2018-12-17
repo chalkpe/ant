03dc2fda539e87f099c707754f0ec7db82f93a19
Chalk <chalkpe@gmail.com>
2018-12-17 15:48:17 +0900

    fix error

--- src/mover.mjs
+++ src/mover.mjs
@@ -1,11 +1,12 @@
 import { join } from 'path'
-import move from 'move-file'
+import mv from 'move-file'
 
 export default async function move ({ target }, res) {
-  const dst = join(target.path, p, res.filename)
   const p = Object.entries(res)
     .reduce((r, [k, v]) => r.replace(`[${k}]`, v), target.rule)
 
-  await move(res.path, dst)
+  const dst = join(target.path, p, res.filename)
+
+  await mv(res.path, dst)
   console.log('moved', res.path, 'to', dst)
 }
